d925b0bd4f85980093955da21f3b536a06986e09
Tobi <tobiasbachle@gmail.com>
2021-03-15 10:28:36 +0100

    Initial commit

---
+++ src/main/java/com/example/tronalddump/TronalddumpApplication.java
@@ -0,0 +1,13 @@
+package com.example.tronalddump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TronalddumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TronalddumpApplication.class, args);
+	}
+
+}
